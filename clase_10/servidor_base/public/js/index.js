86748709e38f73f5101d9d6829447b19833a425d
Joaquin Benitez <joaquincdelu@gmail.com>
2023-03-09 18:45:36 -0300

    entregable_5

--- clase_10/servidor_base/public/js/index.js
+++ clase_10/servidor_base/public/js/index.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const socket = io();
+
+socket.emit("message", "Mensaje desde frontend!");
+
+socket.on("message", (data) => {
+  console.log(data);
+});
+
+socket.on("input-changed", (data) => {
+  const receivedTextInput = document.getElementById("received-text-input");
+  receivedTextInput.innerHTML = data;
+});
+
+const textInput = document.getElementById("text-input");
+textInput.addEventListener("input", (ev) => {
+  socket.emit("input-changed", ev.target.value);
+});
--- clase_10/servidor_base/src/app.js
+++ clase_10/servidor_base/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { engine } from "express-handlebars";
 import __dirname from "./utils.js";
 import viewsRouter from "./routes/views.router.js";
+import { Server } from "socket.io";
 
 const app = express();
 
@@ -13,6 +14,29 @@ app.use(express.static(__dirname + "/../public"));
 
 app.use("/", viewsRouter);
 
-app.listen(8080, () => {
+const httpServer = app.listen(8080, () => {
   console.log("Server listening on port 8080");
+});
+
+const socketServer = new Server(httpServer);
+
+socketServer.on("connection", (socket) => {
+  console.log("New client connected!");
+
+  socket.on("message", (data) => {
+    console.log(data);
+
+    socket.emit("message", "Mensaje enviado desde el servidor!");
+    // socket.broadcast.emit("message", "Mensaje broadcasteado!");
+    // socketServer.emit("message", "Mensaje global!");
+  });
+
+  socket.on("input-changed", (data) => {
+    console.log(data);
+    socketServer.emit("input-changed", data);
+  });
+
+  // setInterval(() => {
+  //   socket.emit("message", "Información actualizada");
+  // }, 1000);
 });
